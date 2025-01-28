e5102b46e7148b43d6e6819f0b7c5e4398f391cd
darshantalati23 <darshantalati23@gmail.com>
2025-01-28 17:18:34 +0530

    New Prompt

--- GameCode/simpleGame.cpp
+++ GameCode/simpleGame.cpp
@@ -1,192 +1,178 @@
 #include <iostream>
 #include <vector>
-#include <thread>
-#include <chrono>
-#include <algorithm>
-#include <termios.h>
+#include <cstdlib>
+#include <ctime>
 #include <unistd.h>
-#include <fcntl.h>
-
+#include <termios.h>
 using namespace std;
 
-// Function to enable non-blocking input
+// Function to capture non-blocking keypress
 char getInput() {
-    struct termios oldt, newt;
+    static struct termios oldt, newt;
     char ch;
-    int oldf;
-
-    tcgetattr(STDIN_FILENO, &oldt); // Save old settings
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO); // Disable buffering and echo
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    ch = getchar();
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // Restore old settings
-
-    return ch;
-}
-
-// Check if a key has been pressed
-bool kbhit() {
-    struct termios oldt, newt;
-    int ch;
-    int oldf;
-
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
     newt.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
     ch = getchar();
-
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-    if (ch != EOF) {
-        ungetc(ch, stdin);
-        return true;
-    }
-
-    return false;
+    return ch;
 }
 
-// Class representing the game board
-class GameBoard {
-    int width, height;
-public:
-    GameBoard(int w, int h) : width(w), height(h) {}
+// Game dimensions
+const int WIDTH = 20;
+const int HEIGHT = 10;
 
-    void draw(const vector<pair<int, int>> &snake, pair<int, int> &food) {
+// Directions
+enum Direction { STOP = 0, UP, DOWN, LEFT, RIGHT };
+
+class SnakeGame {
+private:
+    bool gameOver;
+    int headX, headY;
+    vector<pair<int, int>> snake;
+    int fruitX, fruitY;
+    Direction dir;
+    int score;
+
+public:
+    SnakeGame() {
+        resetGame();
+    }
+
+    void resetGame() {
+        gameOver = false;
+        dir = STOP;
+        headX = WIDTH / 2;
+        headY = HEIGHT / 2;
+
+        // Initialize snake with length 3
+        snake = {{headX, headY}, {headX - 1, headY}, {headX - 2, headY}};
+        score = 0;
+
+        spawnFruit();
+    }
+
+    void spawnFruit() {
+        fruitX = rand() % WIDTH;
+        fruitY = rand() % HEIGHT;
+    }
+
+    void draw() {
         system("clear");
-        for (int i = 0; i < height; ++i) {
-            for (int j = 0; j < width; ++j) {
-                if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
-                    cout << "#"; // Border
-                } else if (make_pair(i, j) == food) {
-                    cout << "F"; // Food
+
+        // Top border
+        for (int i = 0; i < WIDTH + 2; i++) cout << "#";
+        cout << endl;
+
+        // Board content
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH + 2; x++) {
+                if (x == 0 || x == WIDTH + 1) {
+                    cout << "#"; // Borders
+                } else if (x == headX && y == headY) {
+                    cout << "O"; // Snake head
+                } else if (x == fruitX && y == fruitY) {
+                    cout << "F"; // Fruit
                 } else {
                     bool isBody = false;
                     for (auto &segment : snake) {
-                        if (segment == make_pair(i, j)) {
+                        if (segment.first == x && segment.second == y) {
+                            cout << "o";
                             isBody = true;
                             break;
                         }
                     }
-                    cout << (isBody ? "O" : " "); // Snake or empty space
+                    if (!isBody) cout << " ";
                 }
             }
             cout << endl;
         }
+
+        // Bottom border
+        for (int i = 0; i < WIDTH + 2; i++) cout << "#";
+        cout << endl;
+
+        cout << "Score: " << score << endl;
+        cout << "Use W/A/S/D to move. Press X to quit." << endl;
     }
 
-    int getWidth() { return width; }
-    int getHeight() { return height; }
-};
-
-// Class representing the snake
-class Snake {
-    vector<pair<int, int>> body;
-    char direction;
-public:
-    Snake(int x, int y) {
-        body.push_back({x, y});
-        direction = 'D'; // Default direction
+    void input() {
+        if (cin.rdbuf()->in_avail() > 0) {
+            char key = getInput();
+            switch (key) {
+                case 'w':
+                case 'W':
+                    if (dir != DOWN) dir = UP;
+                    break;
+                case 's':
+                case 'S':
+                    if (dir != UP) dir = DOWN;
+                    break;
+                case 'a':
+                case 'A':
+                    if (dir != RIGHT) dir = LEFT;
+                    break;
+                case 'd':
+                case 'D':
+                    if (dir != LEFT) dir = RIGHT;
+                    break;
+                case 'x':
+                case 'X':
+                    gameOver = true;
+                    break;
+            }
+        }
     }
 
-    void move(char dir) {
-        direction = dir;
-        pair<int, int> head = body.front();
-
-        if (direction == 'W') head.first--;      // Up
-        else if (direction == 'S') head.first++; // Down
-        else if (direction == 'A') head.second--;// Left
-        else if (direction == 'D') head.second++;// Right
-
-        body.insert(body.begin(), head);
-        body.pop_back(); // Remove the tail
-    }
-
-    void grow() {
-        body.push_back(body.back());
-    }
-
-    bool checkCollision(int width, int height) {
-        // Check wall collision
-        auto head = body.front();
-        if (head.first <= 0 || head.first >= height - 1 ||
-            head.second <= 0 || head.second >= width - 1) {
-            return true;
+    void logic() {
+        // Move the body
+        for (int i = snake.size() - 1; i > 0; i--) {
+            snake[i] = snake[i - 1];
         }
 
-        // Check self-collision
-        for (size_t i = 1; i < body.size(); ++i) {
-            if (body[i] == head) return true;
+        // Move the head
+        if (dir == UP) headY--;
+        else if (dir == DOWN) headY++;
+        else if (dir == LEFT) headX--;
+        else if (dir == RIGHT) headX++;
+
+        snake[0] = {headX, headY};
+
+        // Check collision with walls
+        if (headX < 0 || headX >= WIDTH || headY < 0 || headY >= HEIGHT) {
+            gameOver = true;
         }
 
-        return false;
-    }
-
-    bool eatFood(pair<int, int> &food) {
-        if (body.front() == food) {
-            grow();
-            return true;
-        }
-        return false;
-    }
-
-    vector<pair<int, int>> getBody() {
-        return body;
-    }
-};
-
-// Generate new food position
-pair<int, int> generateFood(int width, int height, const vector<pair<int, int>> &snake) {
-    pair<int, int> food;
-    do {
-        food.first = rand() % (height - 2) + 1;
-        food.second = rand() % (width - 2) + 1;
-    } while (find(snake.begin(), snake.end(), food) != snake.end());
-    return food;
-}
-
-int main() {
-    srand(time(0));
-    const int width = 20, height = 10;
-    GameBoard board(width, height);
-    Snake snake(height / 2, width / 2);
-    pair<int, int> food = generateFood(width, height, snake.getBody());
-    char input = 'D'; // Initial direction
-
-    while (true) {
-        board.draw(snake.getBody(), food);
-
-        if (kbhit()) {
-            char newInput = getInput();
-            if (newInput == 'W' || newInput == 'A' || newInput == 'S' || newInput == 'D') {
-                input = newInput;
+        // Check collision with itself
+        for (size_t i = 1; i < snake.size(); i++) {
+            if (snake[i].first == headX && snake[i].second == headY) {
+                gameOver = true;
             }
         }
 
-        snake.move(input);
-
-        if (snake.eatFood(food)) {
-            food = generateFood(width, height, snake.getBody());
+        // Check if fruit is eaten
+        if (headX == fruitX && headY == fruitY) {
+            score += 10;
+            snake.push_back({-1, -1}); // Add a new segment
+            spawnFruit();
         }
-
-        if (snake.checkCollision(width, height)) {
-            cout << "Game Over! Final Score: " << snake.getBody().size() - 1 << endl;
-            break;
-        }
-
-        this_thread::sleep_for(chrono::milliseconds(200)); // Control game speed
     }
 
+    void run() {
+        while (!gameOver) {
+            draw();
+            input();
+            logic();
+            usleep(150000); // Slow the game loop
+        }
+        cout << "Game Over! Final Score: " << score << endl;
+    }
+};
+
+int main() {
+    srand(time(0));
+    SnakeGame game;
+    game.run();
     return 0;
 }
-
-/*
-To compile the code, run the following command:
-g++ simpleGame.cpp -o simpleGame
-./simpleGame
-*/
